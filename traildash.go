01055785010204831a63f5e33ec963fff95b121f
Bill Moritz <billmoritz@gmail.com>
2015-11-11 23:30:43 +0000

    Wrong path

--- traildash.go
+++ traildash.go
@@ -6,7 +6,7 @@ import (
 	"flag"
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/session"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/sqs"
 	"io"
